cd2bc2e243567c78e585f35a02052e2ac8960049
Nazilya Mullagildina <mullagildina.n@yandex.ru>
2022-12-07 22:38:05 +0500

    дуыlesson9

---
+++ src/test/java/guru/qa/AttachmentsTest.java
@@ -0,0 +1,40 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class AttachmentsTest {
+    static final String URL = "https://github.com";
+    private final static String REPOSITORY = "eroshenkoam/allure-example";
+    final static String ISSUE = "80";
+
+    @Test
+    public void testLambdaAttachmentStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open(URL);
+            attachment("Source", webdriver().driver().source());
+        });
+
+
+
+    }
+    @Test
+    public void testAnnotatedAttachmentStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps webSteps = new WebSteps();
+
+        webSteps.openMainPage(URL);
+        webSteps.takeScreenshot();
+
+    }
+}
---
+++ src/test/java/guru/qa/StepsTest.java
@@ -0,0 +1,55 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest {
+    static final String URL = "https://github.com";
+    private final static String REPOSITORY = "Nazilya/lesson9_allure-reports";
+    final static String ISSUE = "lesson9-allureTests";
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open(URL);
+        });
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Открываем таб Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверяем наличие Issue с номером " + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+
+    }
+    @Test
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps webSteps = new WebSteps();
+
+        webSteps.openMainPage(URL);
+        webSteps.searchRepo(REPOSITORY);
+        webSteps.openRepo(REPOSITORY);
+        webSteps.openIssueTab();
+        webSteps.checkIssueByName(ISSUE);
+
+
+    }
+}
